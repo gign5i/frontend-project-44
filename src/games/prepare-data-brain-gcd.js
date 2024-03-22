744049ff5d91122b239ab5c5f9f2de980381d32a
Benamgkhar <swsbbnml@gmail.com>
2024-03-22 21:20:16 +0300

    fix in src/games/prepare-data-brain-gcd.js

--- src/games/prepare-data-brain-gcd.js
+++ src/games/prepare-data-brain-gcd.js
@@ -2,7 +2,7 @@ export default () => {
   let a = Math.floor(Math.random() * (10 - 1) + 1); // исключаем 0, поэтому от 1 - 10
   let b = Math.floor(Math.random() * (10 - 1) + 1); // исключаем 0, поэтому от 1 - 10
   let rightAnswer = '';
-  const question = `(${a} , ${b})`;
+  const question = `${a} ${b}`;
 
   while (a !== b) {
     if (a > b) {
